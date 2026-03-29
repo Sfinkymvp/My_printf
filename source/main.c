0a1cc0cec98838264c5f73e6497079440a04ebdd
sfinkymvp <sfinkymvp@gmail.com>
2026-03-29 21:17:30 +0300

    Added specificator %x

--- source/main.c
+++ source/main.c
@@ -3,6 +3,8 @@ extern void my_printf(const char* format, ...);
 
 int main()
 {
-    my_printf("Hello, Aboba! Symbol: %c", 'x');
+    my_printf("Hello, Aboba!\nSymbol: %c\nHex digit: %x\n", 'x', 0x12);
+    my_printf("\nIdi nahyi, digit %x\n", 0xfabcdefff);
+
     return 0;
 }
--- source/my_printf.asm
+++ source/my_printf.asm
@@ -1,13 +1,21 @@
+section .data
+
+digits              db "0123456789ABCDEF"
+
+section .bss
+
+buffer              resb BUFFER_SIZE
+
+
 section .text
     global my_printf
-
 
 BUFFER_SIZE         equ 128
 STDOUT_FD           equ 1
 
 
 my_printf:
-    pop r10                             ; Сохраняем адрес возврата
+    pop r15                             ; Сохраняем адрес возврата
 
     push    r9                          ; 6-й аргумент
     push    r8                          ; 5-й аргумент
@@ -21,7 +29,7 @@ my_printf:
     call    my_printf_logic
 
     add     rsp, 56
-    push    r10                         ; Кладем обратно адрес возврата
+    push    r15                         ; Кладем обратно адрес возврата
 
     ret
 
@@ -82,8 +90,8 @@ my_printf_logic:
     cmp     byte [r12 + rcx], 0x00
     je      .done
 
-    ; cmp     [rdi + rcx], '\'
-    ; je      .escape_handler
+    cmp     byte [r12 + rcx], '\'
+    je      .escape_handler
 
     cmp     byte [r12 + rcx], '%'
     je      .spec_handler
@@ -107,8 +115,7 @@ my_printf_logic:
 .char_handler:
     xor     eax, eax
 
-    xor     r8d, r8d
-    mov     r8b, byte [rbx]             ; Получаем символ
+    movzx   r8, byte [rbx]             ; Получаем символ
     add     rbx, 8
 
     mov     byte [r13 + rdx], r8b       ; Кладем в буфер символ
@@ -119,24 +126,47 @@ my_printf_logic:
 .hex_handler:
     xor     eax, eax
 
-    xor     r8d, r8d
     mov     r8, [rbx]                   ; Получаем число
     add     rbx, 8
 
+    push    rcx
+
     mov     rdi, r8
     mov     rsi, 16                     ; Указываем систему счисления для записи числа
+    mov     rcx, rdx
+    mov     rdx, buffer
 
     sub     rsp, 8
 
     call    itoa
+    add     rsp, 8
+
+    pop     rcx
+
+    inc     rcx
+    mov     rdx, rax
 
     jmp     .continue
+
+.escape_handler:
+    inc rcx
+
+    cmp    byte [r12 + rcx], 'n'
+    je      .line_feed
+
+.line_feed:
+    mov     byte [r13 + rdx], 10
+
+    inc rcx
+    inc rdx
+
+    jmp .continue
 
 .continue:
     jmp     .next_loop
 
 .done:
-    mov     byte [r13 + rdx], 0x00      ; Заканчиваем строку ноль-терминатором
+    mov     byte [r13 + rdx], 0         ; Заканчиваем строку ноль-терминатором
     inc rdx                             ; Учитываем ноль-терминатор в длине строки
 
     push    rdx
@@ -155,6 +185,7 @@ my_printf_logic:
 
     pop     rbp
 ; --- End epilogue --- 
+
     ret
 
 
@@ -165,7 +196,7 @@ my_printf_logic:
 ;       Выводит содержимое буфера в стандартный поток вывода
 ; Входные параметры:
 ;       rdi - Указатель на буфер
-;       rsi - Количество символов для вывода
+;       rsi - Количество элементов в буфере
 ; Выходные параметры:
 ;       Нет
 ; Портящиеся регистры:
@@ -189,17 +220,169 @@ print_buffer:
 ;       Осуществляет запись числа в буфер в необходимой системе счисления
 ; Входные параметры:
 ;       rdi - Число
-;       rsi - Основание системы счисления (Оно должно быть степенью двойки)
+;       rsi - Основание системы счисления (2, 8, 10, 16)
+;       rdx - Указатель на буфер
+;       rcx - Количество элементов в буфере
 ; Выходные параметры:
-;       Нет
+;       rax - Итоговое количество элементов в буфере
 ; Портящиеся регистры:
 ;       callee-caller
 ; -----------------------------------------------------------------------------
 itoa:
+; --- Start prologue ---
+    push    rbp
+    mov     rbp, rsp
+
+    push    rbx
+; --- End prologue ---
+
+    mov     r10, rdi                    ; Сохраняем число
+    mov     r11, rcx                    ; Сохраняем количество элементов в буфере
+
+    cmp     rsi, 10
+    je      .decimal_logic              ; 10-я СС обрабатывается отдельно
+
+    push    rdx                         ; Сохраняем содержимое регистра rdx
+
+;   В регистре r8 будет храниться шаг для битового сдвига. Инструкция 
+;   bsr найдет индекс наибольшего единичного бита в rsi
+    bsr     r8, rsi
+
+    xor     edx, edx
+    mov     rax, 64                     ; 64 - количество бит в регистрах r**
+
+;   В регистре rax будет храниться количество разрядов числа в заданной 
+;   системе счисления. Делимое лежит в rdx:rax, делитель в r8.
+;   rax = 64 / r8
+;   rdx = 64 % r8
+    div     r8
+
+    cmp     rdx, 0
+    je      .skip_inc
+
+;   Если после нахождение шага битового сдвига число 64 поделилось не нацело,
+;   то в таком случае надо учесть неполный разряд, который остался.
+    inc     rax
+
+.skip_inc:
+    mov     rbx, rax
+
+    pop     rdx
+
+    mov     r9, rsi
+    dec     r9                          ; r9 = base - 1 - битовая маска
+
+    mov     rcx, r11
+    add     rcx, rbx
+    cmp     rcx, BUFFER_SIZE
+
+;   Если в буфере достаточно места для числа, то мы не очищаем его
+    jb      .skip_print
+
+    mov     rdi, rdx 
+    mov     rsi, r11
+
+    sub     rsp, 8
+
+    call    print_buffer
+
+    add     rsp, 8
+
+    xor     r11d, r11d                  ; Обнуляем количество элементов в буфере
+
+.skip_print:
+
+    mov     rax, rbx
+
+    jmp     .condition_1
+.loop_1:
+    mov     rsi, r10
+    and     rsi, r9
+    push    rsi
+
+    mov     rcx, r8
+    shr     r10, cl
+
+    dec     rax
+
+.condition_1:
+    cmp     rax, 0
+    jne     .loop_1
+
+    mov     rax, rbx
+
+    jmp     .condition_2
+.loop_2:
+    pop     rsi
+    movzx   rsi, byte [digits + rsi]
+
+    cmp     rsi, '0'
+    je      .skip_load_into_buffer
+
+    mov     byte [rdx + r11], sil
+
+    inc     r11
+
+.skip_load_into_buffer:
+    dec     rax
+
+.condition_2:
+    cmp     rax, 0
+    jne     .loop_2
+
+;   Возвращаем из функции текущее количество элементов в буфере
+    mov     rax, r11
+
+.decimal_logic:
+; --- Start prologue ---
+    pop     rbx
+
+    pop     rbp
+; --- End prologue ---
+
+    ret 
 
 
-section .bss
+; -----
+;     псевдокод:
+;     if ($rsi == 2) {
+;         roll_size = 1
+;     } elif ($rsi == 8) {
+;         roll_size = 3   
+;     } elif ($rsi == 16) {
+;         roll_size = 4
+;     }
+;     and_num = $rsi - 1
 
-buffer              resb BUFFER_SIZE
+;     i = 64 / roll_size
+;     if (64 % roll_size != 0) {
+;         i++
+;     }
+    
+;     ; Если число не поместится в буфер, то очистим содержимое буфера
+;     if (idx + i >= BUFFER_SIZE) {
+;         print_buffer(buffer, idx)
+;         idx = 0
+;     }
+
+;     k = i
+;     while (k != 0) {
+;         ; Example for oct notation
+;         num = $rdi
+;         and num, and_num  ; 0x0111
+;         push num + '0'
+;         $rdi >>= roll_size
+;         k--
+;     }
+
+;     k = i
+;     while (i != 0) {
+;         pop rdx
+;         buffer[idx] = rdx
+;         idx++
+;     }
+
+;     return idx 
+
 
 section .note.GNU-stack
